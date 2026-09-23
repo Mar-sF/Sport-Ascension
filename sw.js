d65277af5ccb5a6b1b75181a8d058ce6e370e639
Mar-sF <marvin59170@gmail.com>
2026-09-23 13:13:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Sport Ascension — Service Worker v25 (network-first pour l'app)
-var CACHE = 'ascension-v31';
+var CACHE = 'ascension-v33';
 
 self.addEventListener('install', function(e) {
   self.skipWaiting();
